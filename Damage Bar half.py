48dab6d9e4e8590176398823d640d041d4679572
Wilf233 <148545913+Wilf233@users.noreply.github.com>
2025-03-29 17:30:50 +0800

    Update Damage Bar half.py

--- Damage Bar half.py
+++ Damage Bar half.py
@@ -4,7 +4,7 @@ import math
 
 # 此程序用于批量在Invicon上添加一半耐久的耐久条
 
-durability = 25 # 物品的最大耐久度
+max_durability = 25 # 物品的最大耐久度
 folder_path = "32px"  # 文件夹路径
 
 def hsv_to_rgb(h, s, v):
@@ -35,23 +35,27 @@ def hsv_to_rgb(h, s, v):
     
     return r, g, b
 
-h, s, v = math.ceil(0.5 * durability) / durability * 120, 1, 1
+h, s, v = math.ceil(0.5 * max_durability) / max_durability * 120, 1, 1
 r, g, b = hsv_to_rgb(h, s, v)
 
 for filename in os.listdir(folder_path):
     file_path = os.path.join(folder_path, filename)
 
-    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-        try:
-            image = Image.open(file_path).convert("RGBA")
-            draw = ImageDraw.Draw(image)
+    if max_durability > 0:
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            try:
+                image = Image.open(file_path).convert("RGBA")
+                draw = ImageDraw.Draw(image)
 
-            draw.rectangle([(4,26), (29,29)], fill=(0, 0, 0, 255))
-            draw.rectangle([(4,26), (17,27)], fill=(r, g, b, 255))
+                draw.rectangle([(4,26), (29,29)], fill=(0, 0, 0, 255))
+                draw.rectangle([(4,26), (17,27)], fill=(r, g, b, 255))
 
-            image.save(os.path.join(folder_path, filename.replace("Invicon", "Invicon Damaged")))
-            print(f"已处理：{filename}")
-        
-        except Exception as e:
-            print(f"处理图片 {filename} 时出错: {e}")
+                image.save(os.path.join(folder_path, filename.replace("Invicon", "Invicon Damaged")))
+                print(f"已处理：{filename}")
+            
+            except Exception as e:
+                print(f"处理图片 {filename} 时出错: {e}")
+    else:
+        print("最大耐久度不合法")
+        break
 
