c00c587cbea09c173d7fab4a01b67d1984cf5dd7
Wilf233 <148545913+Wilf233@users.noreply.github.com>
2025-04-02 15:34:07 +0800

    Add files via upload

--- Generate Glint Inv Image.bat
+++ Generate Glint Inv Image.bat
@@ -1,3 +1,3 @@
 cd C:\Users\34395\Desktop\Image Rendering\Cropped Images
-ffmpeg -i Invicon_%%d.png -vf palettegen=reserve_transparent=1 palette.png
-ffmpeg -i Invicon_%%d.png -framerate 20 -i palette.png -lavfi paletteuse=alpha_threshold=128 -gifflags -offsetting inv.gif
+ffmpeg -i Invicon_%%d.png -framerate 20 -vf palettegen=reserve_transparent=1 palette.png
+ffmpeg -framerate 20 -i Invicon_%%d.png -i palette.png -lavfi "paletteuse=alpha_threshold=128" -r 20 inv.gif
--- Generate Glint Item Image.bat
+++ Generate Glint Item Image.bat
@@ -1,3 +1,3 @@
 cd C:\Users\34395\Desktop\Image Rendering\Cropped Images
-ffmpeg -i item_image_%%d.png -vf palettegen=reserve_transparent=1 palette.png
-ffmpeg -i item_image_%%d.png -framerate 20 -i palette.png -lavfi paletteuse=alpha_threshold=128 -gifflags -offsetting item_image.gif
+ffmpeg -i item_image_%%d.png -framerate 20 -vf palettegen=reserve_transparent=1 palette.png
+ffmpeg -framerate 20 -i item_image_%%d.png -i palette.png -lavfi "paletteuse=alpha_threshold=128" -r 20 inv.gif
